fc0e78252419087bb873c703b1204bbd24438ec3
brunoguerra <brunoguerra@hotmail.com>
2023-09-15 09:10:24 -0300

    feat: incluído getter na classe Contador

--- Contador.java
+++ Contador.java
@@ -1,7 +1,11 @@
 public class Contador {
-  private int valor = 0;
+    private int valor = 0;
 
-  public synchronized void incrementar() {
-      valor++;
-  }
+    public synchronized void incrementar() {
+        valor++;
+    }
+
+    public int getValor() {
+        return valor;
+    }  
 }
